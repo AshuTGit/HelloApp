9c389826f9382ef4f8c515034f8df6a10043e9a3
Tiwari <172633@NTTDATA.COM>
2019-08-13 15:53:24 +0530

    Initial version checkin

---
+++ HelloApp/src/main/java/com/hello/example/HelloApp/controller/HelloResrController.java
@@ -0,0 +1,14 @@
+package com.hello.example.HelloApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/hello")
+public class HelloResrController {
+	@GetMapping(value="/msg")
+public String getMessage() {
+	return "Hello 2 All...!!";
+}
+}
